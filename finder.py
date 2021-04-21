a93cda745a34e20d67acdb0bdeb2d260868bee88
Hackdroid <raza.11409652@lpu.in>
2021-04-21 08:04:10 +0530

    Added

--- finder.py
+++ finder.py
@@ -6,25 +6,26 @@ Real Time Face Recogition
 
 import cv2
 import numpy as np
-import os 
+import os
+import requests
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainer/trainer.yml')
 cascadePath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascadePath);
+faceCascade = cv2.CascadeClassifier(cascadePath)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-#iniciate id counter
+# iniciate id counter
 id = 0
 
 # names related to ids: example ==> Marcelo: id=1,  etc
-names = ['None', 'Khalid', 'Shahrukh', 'HEllo', 'Z', 'W'] 
+names = ['None', 'Khalid', 'Manish', 'Indrajeet', 'User 3', 'User 4']
 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video widht
-cam.set(4, 480) # set video height
+cam.set(3, 640)  # set video widht
+cam.set(4, 480)  # set video height
 
 # Define min window size to be recognized as a face
 minW = 0.1*cam.get(3)
@@ -32,38 +33,53 @@ minH = 0.1*cam.get(4)
 
 while True:
 
-    ret, img =cam.read()
-    img = cv2.flip(img, 1) # Flip vertically
+    ret, img = cam.read()
+    img = cv2.flip(img, 1)  # Flip vertically
 
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale( 
+    faces = faceCascade.detectMultiScale(
         gray,
-        scaleFactor = 1.2,
-        minNeighbors = 5,
-        minSize = (int(minW), int(minH)),
-       )
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(int(minW), int(minH)),
+    )
 
-    for(x,y,w,h) in faces:
+    for(x, y, w, h) in faces:
 
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+        id, confidence = recognizer.predict(gray[y:y+h, x:x+w])
 
-        # Check if confidence is less them 100 ==> "0" is perfect match 
+        # Check if confidence is less them 100 ==> "0" is perfect match
         if (confidence < 100):
+            currentuser = id
             id = names[id]
             confidence = "  {0}%".format(round(100 - confidence))
+            data = {
+                "latitude": 24.6,
+                "longitude": 74.6,
+                "address": "LPU Law gate Jalandhar",
+                "time": "2021-04-21 09:01:22 am",
+                "date": "2021-04-21",
+                "type": 1,
+                "userId": currentuser
+            }
+            print(data)
+            response = requests.post(
+                "http://13.126.177.176:7770/attandance/mark/api", data=data)
+            print(response.json())
         else:
             id = "unknown"
             confidence = "  {0}%".format(round(100 - confidence))
-        
-        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
-    
-    cv2.imshow('camera',img) 
 
-    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+        cv2.putText(img, str(id), (x+5, y-5), font, 1, (255, 255, 255), 2)
+        cv2.putText(img, str(confidence), (x+5, y+h-5),
+                    font, 1, (255, 255, 0), 1)
+
+    cv2.imshow('camera', img)
+
+    k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
     if k == 27:
         break
 
